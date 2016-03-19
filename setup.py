2ecdf1e623a5ab1be573c3a33758cee5df5af510
Phivos Stylianides <stphivos@gmail.com>
2016-03-19 04:14:09 +0200

    Add package v0.1 to pypi

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='fnval',
+    packages=['fnval'],
+    version='0.1',
+    description='A missing function level access control checker',
+    author='Phivos Stylianides',
+    author_email='stphivos@gmail.com',
+    url='https://github.com/stphivos/fnval',
+    keywords=['access control', 'hardening', 'security', 'vulnerability scanning'],
+    classifiers=[],
+)
